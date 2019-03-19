56ccd1369696e9d43b0229e2c954b7dbe2a564ba
Gabi Stanovsky <gabis@julianm.local>
2019-03-19 16:43:14 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# DROP-leaderboard-example
+
+This repository is an example of a [DROP leaderboard](https://leaderboard.allenai.org/drop/submissions/public).
+Specifically, We wrap the NAQANET model (described in the [DROP paper](https://arxiv.org/abs/1903.00161) in a [Docker](https://www.docker.com) container and publish it as a [beaker](https://beaker.org/) blueprint, which can then be submitted as a leaderboard entry.
+
+## Requirements
+
+* [Docker](https://www.docker.com)
+* [beaker](https://beaker.org/)
+
+## Submitting to the leaderboard
+
+Follow the following steps from the repository root directory:
+
+1. Create a docker image:
+
+        docker build -t naqanet .
+
+2. Upload a blueprint to beaker:
+
+        beaker blueprint create -n my-naqanet-blueprint naqanet:latest
+
+**NOTE:** The blueprint name (e.g., `my-naqanet-blueprint`) should be unique in beaker.
+
+3. Make a submission:
+   * Follow the steps on the [DROP leaderboard](https://leaderboard.allenai.org/drop/submissions/public) to gain access to making submissions.
+   * On the new submission form, make sure to enter your blueprint name (e.g., `my-naqanet-blueprint`).
+
+
+
---
+++ predict.py
@@ -0,0 +1,43 @@
+import argparse
+import json
+from allennlp.predictors import Predictor
+from allennlp.models.archival import load_archive
+from tqdm import tqdm
+
+if __name__ == "__main__":
+    # Parse arguments
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--archive_file", type = str, required = True,
+                        help = "URL for a trained model file")
+    parser.add_argument("--input_file", type=str, required=True,
+                        help='path for drop input files')
+    parser.add_argument("--output_file", type=str, required=True,
+                        help="path for predictions output file")
+    args = parser.parse_args()
+
+    # Create predictor
+    archive = load_archive(args.archive_file)
+    predictor = Predictor.from_archive(archive, "machine-comprehension")
+
+    predictions = {}
+
+    # Run on input file & collect answers
+    input_json = json.load(open(args.input_file, encoding = "utf8"))
+    passages = input_json.items()
+    for passage_id , passage_data in tqdm(passages):
+        passage = passage_data["passage"]
+        for qa_pair in passage_data["qa_pairs"]:
+            question = qa_pair["question"]
+            query_id = qa_pair["query_id"]
+            prediction = predictor.predict(question = question, passage = passage)
+            answer = prediction["answer"]
+            answer_type = answer["answer_type"]
+            ans_str = str(answer["value"]) \
+                      if (answer_type != "count")  \
+                         else str(answer["count"])
+            predictions[query_id] = ans_str
+
+    # Write output file
+    with open(args.output_file, "w", encoding = "utf8") as fout:
+        json.dump(predictions, fout)
